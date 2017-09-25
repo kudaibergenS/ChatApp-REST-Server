a4ed0f6ed3254808d09637fe00e86a5324e8d8bb
Sanzhar Kydaybergen <sanzhar.k.96@gmail.com>
2017-09-26 01:24:11 +0600

    "annotation configured server mockup"

--- src/main/java/com/sanzhar/chatappserver/controller/UserController.java
+++ src/main/java/com/sanzhar/chatappserver/controller/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller("/users")
+@Controller
 public class UserController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
