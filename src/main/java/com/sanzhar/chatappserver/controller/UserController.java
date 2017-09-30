d5265ae32f6c77c5ee26c60e4d3546d129a0d7f8
Sanzhar Kydaybergen <sanzhar.k.96@gmail.com>
2017-09-30 21:50:30 +0600

    "annotation configured server mockup"

--- src/main/java/com/sanzhar/chatappserver/controller/UserController.java
+++ src/main/java/com/sanzhar/chatappserver/controller/UserController.java
@@ -5,12 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
+@RequestMapping("user")
 public class UserController {
 
-    @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public @ResponseBody String getUsers() {
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public @ResponseBody Map test() {
+        Map user = new HashMap();
+        user.put("name", "test");
+        user.put("age", 21);
 
-    return "test...";
+    return user;
     }
+
 }
